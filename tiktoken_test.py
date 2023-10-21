4a1de47ef521661415c81b93372fa8eb97b232a3
psymbio <psymbio@gmail.com>
2023-10-21 15:58:05 +0530

    small cleanup and edits to index.html

--- tiktoken_test.py
+++ tiktoken_test.py
@@ -1,1 +1,9 @@
+import tiktoken
+enc = tiktoken.get_encoding("cl100k_base")
+if (enc.decode(enc.encode("hello world")) == "hello world"):
+    print("encoding and decoding test passed!")
+else:
+    print("encoding and decoding test failed...")
 
+# To get the tokeniser corresponding to a specific model in the OpenAI API:
+enc = tiktoken.encoding_for_model("gpt-4")
